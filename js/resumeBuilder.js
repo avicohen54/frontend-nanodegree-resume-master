12c756d384bcef1b359ab5a4d35dfce18c8f7a28
Avi Cohen <avi@pacific54.com>
2016-06-27 14:37:16 -0400

    Fronted Nano degree Resume Master
    Storing in Git my homework with Udacity.

---
+++ js/resumeBuilder.js
@@ -0,0 +1,76 @@
+var bio = {
+    "name" : "Avi Cohen",
+    "role" : "Owner",
+    "Contacts" : {
+        "mobile" : "305.457.1090",
+        "email" : "avi@pacific54.com",
+        "github" : "avicohen54",
+        "twitter" : "avicohne54",
+        "location" : "Wynwood, FL"
+    },
+    "welcomeMessage" : "lorem ipsum dolor sit amet etc etc etc",
+    "skills" : [
+        "awesomeness", "delivering things", "extra sleep", "etc"
+    ],
+    "HTMLbioPic": "images/fry.jpg"
+};
+var education = {
+    "schools": [
+        {
+            "name": "Keiser University1",
+            "city": "Fort Lauderdale, FL",
+            "degree": "Masters",
+            "major": ["CS"],
+            "dates": 2013,
+            "url": "http://keiseruniversity.edu"
+        },
+        {
+            "name": "Keiser University2",
+            "city": "Fort Lauderdale, FL",
+            "degree": "Masters",
+            "major": ["CS"],
+            "dates": 2013,
+            "url": "http://keiseruniversity.edu"
+        },
+        {
+            "name": "Keiser University3",
+            "city": "Fort Lauderdale, FL",
+            "degree": "Masters",
+            "major": ["CS"],
+            "dates": 2013,
+            "url": "http://keiseruniversity.edu"
+        }
+    ]
+};
+var work = {
+    "jobs" : [
+        {
+            "employer" : "xyz",
+            "title" : "Job Title",
+            "dates" : "2009-2001",
+            "description" : "Job Description"
+        },
+        {
+            "employer" : "xyz2",
+            "title" : "Job Title",
+            "dates" : "2009-2001",
+            "description" : "Job Description"
+        },
+        {
+            "employer" : "xyz3",
+            "title" : "Job Title",
+            "dates" : "2009-2001",
+            "description" : "Job Description"
+        }
+    ]
+};
+var project = {
+    "project" : [
+        {
+            "project" : "Project Coding",
+            "title" : "learn to code isn't easy!",
+            "dates" : "2016-Today",
+            "description" : "learning a new skill"
+        }
+    ]
+};
